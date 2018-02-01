90b96b1d66464a2378f2d615222c450fdd1c6c42
CHENG Ri Sheng <risheng.cheng@brother.co.jp>
2018-02-01 13:01:29 +0900

    Update prototype to use actual STS API #181 (#184)
    * Update prototype to use actual STS API #181
    
    * Add error logging #181
    
    * Move lamda role ARN environment variable to serverless.yml #181

--- functions/tokens/handler.py
+++ functions/tokens/handler.py
@@ -1,16 +1,35 @@
-import uuid
 import json
-import datetime
+import boto3
+import logging
+import sys
+from botocore.exceptions import ClientError
+from os import environ
+
+logger = logging.getLogger('tokens')
+logger.setLevel(logging.INFO)
 
 
 def get_one_time_token(event, context):
-    token = {
-        'session_token': uuid.uuid4().hex,
-        'secret_access_key': uuid.uuid4().hex,
-        'access_key_id': uuid.uuid4().hex,
-        'expiration': datetime.datetime.now().isoformat()
-    }
-    return {
-        'statusCode': 200,
-        'body': json.dumps(token)
+    client = boto3.client('sts')
+    try:
+        response = client.assume_role(
+            RoleArn=environ['LAMBDA_ROLE_ARN'],
+            RoleSessionName='CMPS'
+            )['Credentials']
+
+        token = {
+            'session_token': response['SessionToken'],
+            'secret_access_key': response['SecretAccessKey'],
+            'access_key_id': response['AccessKeyId'],
+            'expiration': response['Expiration'].strftime('%Y-%m-%dT%H:%M:%S')
         }
+        return {
+            'statusCode': 200,
+            'body': json.dumps(token)
+            }
+    except ClientError as e:
+        logger.error(e)
+        return {'statusCode': 403}
+    except:
+        logger.error(sys.exc_info())
+        return {'statusCode': 500}
--- tests/functions/tokens/test_handler.py
+++ tests/functions/tokens/test_handler.py
@@ -1,13 +1,38 @@
 import unittest
 import json
+import datetime
 from functions.tokens import handler
+from unittest.mock import patch
+from botocore.exceptions import ClientError
+from os import environ
 
 
 class TestOneTimeTokens(unittest.TestCase):
+    def setUp(self):
+        environ['LAMBDA_ROLE_ARN'] = ''
 
-    def test_get_token(self):
+    @patch('boto3.client')
+    def test_get_token_success(self, mock):
+        mock().assume_role.return_value = {
+            'Credentials': {
+                'SessionToken': 'session_token',
+                'SecretAccessKey': 'secret_access_key',
+                'AccessKeyId': 'access_key_id',
+                'Expiration': datetime.datetime.now()
+            }
+        }
         res = json.loads(handler.get_one_time_token('', '')['body'])
         self.assertTrue('session_token' in res)
         self.assertTrue('secret_access_key' in res)
         self.assertTrue('access_key_id' in res)
         self.assertTrue('expiration' in res)
+
+    @patch('boto3.client')
+    def test_get_token_client_error(self, mock):
+        mock().assume_role.side_effect = ClientError(
+            {'Error': {'Code': '403', 'Message': 'Unauthorized'}}, 'AssumeRole'
+        )
+        res = handler.get_one_time_token('', '')
+
+        self.assertTrue('statusCode' in res)
+        self.assertTrue(res['statusCode'], 403)
